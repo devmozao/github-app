01b8ad80de4f3a3f764b0ad1650c0425be753ab4
Diogo Fonseca <devmozao@outlook.com>
2020-05-13 19:11:20 -0300

    added profile routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,12 +5,15 @@ import Loading from '../atomic-design/02-molecules/loading/loading'
 
 const Home = lazy(() => import('../atomic-design/05-pages/home/home'))
 const NotFound = lazy(() => import('../atomic-design/05-pages/not-found/not-found'))
+const Profile = lazy(() => import('../atomic-design/05-pages/profile/profile'))
 
 const Routes = () => {
   return (
     <Suspense fallback={<Loading />}>
       <Router>
         <Switch>
+          <Route path='/profile/:username' component={Profile} />
+          <Route path='/profile/' component={Profile} />
           <Route path='/' component={Home} exact />
           <Route path='*' component={NotFound} />
         </Switch>
