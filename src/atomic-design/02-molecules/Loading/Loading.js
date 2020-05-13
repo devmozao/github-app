36659bef8549d2ce470d3e2c1070550fd9da7f10
Diogo Fonseca <devmozao@outlook.com>
2020-05-13 15:09:33 -0300

    refactored spinner component

--- src/atomic-design/02-molecules/Loading/Loading.js
+++ src/atomic-design/02-molecules/Loading/Loading.js
@@ -1,12 +1,6 @@
 import React, { memo } from 'react'
 
-import styled from 'styled-components'
-
 import Container from '../../01-atoms/Container/Container'
-
-const Spinner = styled.div`
-/* */
-`
 
 const Loading = () => {
   return (
