551d39c9a0cf24d441744a8e6b87ae5c4fe99903
Diogo Fonseca <devmozao@outlook.com>
2020-05-13 15:09:46 -0300

    added spinner atom

---
+++ src/atomic-design/01-atoms/Spinner/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const StyledSpinner = styled.div`
+/* */
+`
+
+const Spinner = () => {
+  return (
+    <StyledSpinner />
+  )
+}
+
+export default Spinner
