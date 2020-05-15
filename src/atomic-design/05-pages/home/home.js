6438664f1f078a8acd3d0775696696caa02d5b54
Diogo Fonseca <devmozao@outlook.com>
2020-05-15 09:34:10 -0300

    removed fragment

--- src/atomic-design/05-pages/home/home.js
+++ src/atomic-design/05-pages/home/home.js
@@ -2,17 +2,15 @@ import React, { memo } from 'react'
 
 import TemplateHome from '../../04-templates/home/home'
 
-function Home ({ history }) {
-  function handleSearch (event) {
+function Home({ history }) {
+  function handleSearch(event) {
     event.preventDefault()
     const username = event.nativeEvent.target[0].value
     history.push(`/profile/${username}`)
   }
 
   return (
-    <>
-      <TemplateHome handleSearch={handleSearch} />
-    </>
+    <TemplateHome handleSearch={handleSearch} />
   )
 }
 
