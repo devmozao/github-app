bc37e3077d2fa0d94b29afe8c0e2d0ca26eadf0d
Diogo Fonseca <devmozao@outlook.com>
2020-05-15 09:45:31 -0300

    removed console.log

--- src/atomic-design/05-pages/not-found/not-found.js
+++ src/atomic-design/05-pages/not-found/not-found.js
@@ -3,10 +3,7 @@ import React, { memo } from 'react'
 import TemplateNotFound from '../../04-templates/not-found/not-found'
 
 function NotFound ({ history }) {
-  console.log('h', history)
-
   function handleRedirect () {
-    console.log('history', history)
     history.push('/')
   }
   return (
