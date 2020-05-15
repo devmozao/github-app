01dd9783259c9957ccf07b426af181efd14e327c
Diogo Fonseca <devmozao@outlook.com>
2020-05-15 02:54:20 -0300

    initialized styled grid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 
+import { BaseCSS } from 'styled-bootstrap-grid'
+
 import Routes from './routes/Routes'
 
 ReactDOM.render(
   <StrictMode>
+    <BaseCSS />
     <Routes />
   </StrictMode>,
   document.getElementById('root')
