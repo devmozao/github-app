768974d0914a6f15b70a592dc2a6657c254728cf
Diogo Fonseca <devmozao@outlook.com>
2020-05-16 17:08:08 -0300

    added mouse hover effects for background color

--- src/atomic-design/01-atoms/button/button.js
+++ src/atomic-design/01-atoms/button/button.js
@@ -2,16 +2,20 @@ import React, { memo } from 'react'
 
 import styled from 'styled-components'
 
+import ColorLuminance from '../../utils/colorLuminance'
+
 const StyledButton = styled.button`
   width: 100px;
   height: 50px;
-  /* border-radius: 2px; */
   border: 2px;
+  &:hover {
+    cursor: pointer;
+  }
 `
-
 const DefaultButton = ({
   icon = '',
   bgColor = '',
+  bgColorHover = '',
   onClick,
   children
 }) => {
@@ -21,8 +25,15 @@ const DefaultButton = ({
     <StyledButton
       onClick={onClick}
       style={{ backgroundColor: bgColor }}
+      onMouseEnter={(e) => { e.target.style.backgroundColor = ColorLuminance(bgColor, -0.2) }}
+      onMouseLeave={(e) => { e.target.style.backgroundColor = bgColor }}
     >
-      {icon && (<Icon />)}
+      {icon && (
+        <Icon
+          onMouseEnter={(e) => { e.target.style.backgroundColor = ColorLuminance(bgColor, -0.2) }}
+          onMouseLeave={(e) => { e.target.style.backgroundColor = bgColor }}
+        />
+      )}
       {children}
     </StyledButton>
   )
