0f7432df6193d4616b32fa5786c51fa781bd2e7f
Diogo Fonseca <devmozao@outlook.com>
2020-05-16 20:51:12 -0300

    added error boundary to routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,18 +6,21 @@ import Loading from '../atomic-design/02-molecules/loading/loading'
 const Home = lazy(() => import('../atomic-design/05-pages/home/home'))
 const NotFound = lazy(() => import('../atomic-design/05-pages/not-found/not-found'))
 const Profile = lazy(() => import('../atomic-design/05-pages/profile/profile'))
+const ErrorBoundary = lazy(() => import('../atomic-design/utils/error-boundary'))
 
 const Routes = () => {
   return (
     <Suspense fallback={<Loading />}>
-      <Router>
-        <Switch>
-          <Route path='/profile/:username' component={Profile} />
-          <Route path='/profile/' component={Profile} />
-          <Route path='/' component={Home} exact />
-          <Route path='*' component={NotFound} />
-        </Switch>
-      </Router>
+      <ErrorBoundary>
+        <Router>
+          <Switch>
+            <Route path='/profile/:username' component={Profile} />
+            <Route path='/profile/' component={Profile} />
+            <Route path='/' component={Home} exact />
+            <Route path='*' component={NotFound} />
+          </Switch>
+        </Router>
+      </ErrorBoundary>
     </Suspense>
   )
 }
