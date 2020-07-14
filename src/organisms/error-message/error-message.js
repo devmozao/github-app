0afa3613c905dbed7bd867b351837f57073d04bd
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-07-14 02:28:03 -0300

    removed styled component dep

--- src/organisms/error-message/error-message.js
+++ src/organisms/error-message/error-message.js
@@ -1,22 +1,17 @@
 import React, { memo } from 'react'
 
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 import NotificationMessage from '../../molecules/notification-message/notification-message'
 
-const StyledErrorMessage = styled.main`
-`
-
 const ErrorMessage = ({ handleRedirect, showRedirect, children }) => {
   return (
-    <StyledErrorMessage>
-      <NotificationMessage
-        handleRedirect={handleRedirect}
-        showRedirect={showRedirect}
-      >
-        {children}
-      </NotificationMessage>
-    </StyledErrorMessage>
+    <NotificationMessage
+      handleRedirect={handleRedirect}
+      showRedirect={showRedirect}
+    >
+      {children}
+    </NotificationMessage>
   )
 }
 
